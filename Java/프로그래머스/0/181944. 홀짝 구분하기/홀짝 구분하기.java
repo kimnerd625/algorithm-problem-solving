e31f4b4ddaa99a07262ab6de9c52deb609ca41ff
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2024-08-03 17:39:02 +0900

    [level 0] Title: 홀짝 구분하기, Time: 146.34 ms, Memory: 68.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        String result = n % 2 == 0 ? String.format("%d is even", n) : String.format("%d is odd", n);
+        System.out.println(result);
+    }
+}
