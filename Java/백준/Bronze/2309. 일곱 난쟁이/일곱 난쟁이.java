de847d8f0b68abf3cf1ba754e4e8305f04fb13bc
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2024-08-07 11:32:21 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 96 ms, Memory: 12924 KB -BaekjoonHub

--- Java/백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ Java/백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,13 +1,13 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] arr = new int[9];
-        List<Integer> result = new ArrayList<>();
         for (int i = 0; i < 9; i++) {
             arr[i] = sc.nextInt();
         }
+        List<Integer> result = new ArrayList<>();
         addSum(arr, 7, 0, 0, result);
     }
     
@@ -15,16 +15,16 @@ public class Main {
         if (n == 0) {
             if (sum == 100) {
                 printResult(result);
-                return true;
+                return true; // 결과를 찾으면 재귀 호출 종료
             }
-            return false;
+            return false; // n이 0이 되었지만 sum이 100이 아닌 경우
         } else {
             for (int i = idx; i < arr.length; i++) {
                 result.add(arr[i]);
                 if (addSum(arr, n - 1, sum + arr[i], i + 1, result)) {
-                    return true;
+                    return true; // 결과를 찾으면 재귀 호출 종료
                 }
-                result.remove(result.size() - 1);
+                result.remove(result.size() - 1); // 정확한 요소 제거
             }
         }
         return false;
